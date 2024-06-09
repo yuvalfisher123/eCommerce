c9fea5a0733aec0c2584a2101613d7e4299030c7
Yuval Fisher <yuvalyfisher@gmail.com>
2024-06-09 18:08:13 +0300

    feat(creditCardService.ts): adding crud for creditcard

--- back/src/services/orderService.ts
+++ back/src/services/orderService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, User } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { Request, Response } from "express";
 
 const orderRepository = new PrismaClient().order;
